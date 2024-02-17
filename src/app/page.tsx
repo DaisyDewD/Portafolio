f318e2dbbcb66ec070b343384e7c77fd7de25299
“DaisyDew” <d.dew.design@gmail.com>
2024-02-17 14:24:51 -0500

    home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,121 +1,64 @@
-import ImageFallback from "@/helpers/ImageFallback";
-import { getListPage } from "@/lib/contentParser";
-import { markdownify } from "@/lib/utils/textConverter";
-import CallToAction from "@/partials/CallToAction";
+import BlogCard from "@/components/BlogCard";
+import Pagination from "@/components/Pagination";
+import config from "@/config/config.json";
+import { getListPage, getSinglePage } from "@/lib/contentParser";
+import { getAllTaxonomy, getTaxonomy } from "@/lib/taxonomyParser";
+import { sortByDate } from "@/lib/utils/sortFunctions";
+import PageHeader from "@/partials/PageHeader";
+import PostSidebar from "@/partials/PostSidebar";
 import SeoMeta from "@/partials/SeoMeta";
-import Testimonials from "@/partials/Testimonials";
-import { Button, Feature } from "@/types";
-import Link from "next/link";
-import { FaCheck } from "react-icons/fa/index.js";
+import { Post } from "@/types";
+const { blog_folder, pagination } = config.settings;
 
-const Home = () => {
-  const homepage = getListPage("homepage/_index.md");
-  const testimonial = getListPage("sections/testimonial.md");
-  const callToAction = getListPage("sections/call-to-action.md");
-  const { frontmatter } = homepage;
-  const {
-    banner,
-    features,
-  }: {
-    banner: { title: string; image: string; content?: string; button?: Button };
-    features: Feature[];
-  } = frontmatter;
+// for all regular pages
+const Posts = () => {
+  const postIndex: Post = getListPage(`${blog_folder}/_index.md`);
+  const { title, meta_title, description, image } = postIndex.frontmatter;
+  const posts: Post[] = getSinglePage(blog_folder);
+  const allCategories = getAllTaxonomy(blog_folder, "categories");
+  const categories = getTaxonomy(blog_folder, "categories");
+  const tags = getTaxonomy(blog_folder, "tags");
+  const sortedPosts = sortByDate(posts);
+  const totalPages = Math.ceil(posts.length / pagination);
+  const currentPosts = sortedPosts.slice(0, pagination);
 
   return (
     <>
-      <SeoMeta />
-      <section className="section pt-14">
+      <SeoMeta
+        title={title}
+        meta_title={meta_title}
+        description={description}
+        image={image}
+      />
+      <PageHeader title={postIndex.frontmatter.title} />
+      <section className="section">
         <div className="container">
-          <div className="row justify-center">
-            <div className="mb-16 text-center lg:col-7">
-              <h1
-                className="mb-4"
-                dangerouslySetInnerHTML={markdownify(banner.title)}
-              />
-              <p
-                className="mb-8"
-                dangerouslySetInnerHTML={markdownify(banner.content ?? "")}
-              />
-              {banner.button!.enable && (
-                <Link className="btn btn-primary" href={banner.button!.link}>
-                  {banner.button!.label}
-                </Link>
-              )}
-            </div>
-            {banner.image && (
-              <div className="col-12">
-                <ImageFallback
-                  src={banner.image}
-                  className="mx-auto"
-                  width="800"
-                  height="420"
-                  alt="banner image"
-                  priority
-                />
+          <div className="row gx-5">
+            <div className="lg:col-8">
+              <div className="row">
+                {currentPosts.map((post: any, index: number) => (
+                  <div key={index} className="mb-14 md:col-6">
+                    <BlogCard data={post} />
+                  </div>
+                ))}
               </div>
-            )}
+              <Pagination
+                section={blog_folder}
+                currentPage={1}
+                totalPages={totalPages}
+              />
+            </div>
+
+            <PostSidebar
+              categories={categories}
+              tags={tags}
+              allCategories={allCategories}
+            />
           </div>
         </div>
       </section>
-
-      {features.map((feature, index: number) => (
-        <section
-          key={index}
-          className={`section-sm ${index % 2 === 0 && "bg-gradient"}`}
-        >
-          <div className="container">
-            <div className="row items-center justify-between">
-              <div
-                className={`mb:md-0 mb-6 md:col-5 ${
-                  index % 2 !== 0 && "md:order-2"
-                }`}
-              >
-                <ImageFallback
-                  src={feature.image}
-                  height={480}
-                  width={520}
-                  alt={feature.title}
-                />
-              </div>
-              <div
-                className={`md:col-7 lg:col-6 ${
-                  index % 2 !== 0 && "md:order-1"
-                }`}
-              >
-                <h2
-                  className="mb-4"
-                  dangerouslySetInnerHTML={markdownify(feature.title)}
-                />
-                <p
-                  className="mb-8 text-lg"
-                  dangerouslySetInnerHTML={markdownify(feature.content)}
-                />
-                <ul>
-                  {feature.bulletpoints.map((bullet: string) => (
-                    <li className="relative mb-4 pl-6" key={bullet}>
-                      <FaCheck className={"absolute left-0 top-1.5"} />
-                      <span dangerouslySetInnerHTML={markdownify(bullet)} />
-                    </li>
-                  ))}
-                </ul>
-                {feature.button.enable && (
-                  <Link
-                    className="btn btn-primary mt-5"
-                    href={feature.button.link}
-                  >
-                    {feature.button.label}
-                  </Link>
-                )}
-              </div>
-            </div>
-          </div>
-        </section>
-      ))}
-
-      <Testimonials data={testimonial} />
-      <CallToAction data={callToAction} />
     </>
   );
 };
 
-export default Home;
+export default Posts;
