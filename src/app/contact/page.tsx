f0d98c6526b615e962c7b679628ce3a19340b8d1
“DaisyDew” <d.dew.design@gmail.com>
2024-02-18 15:26:00 -0500

    formspree

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,54 +1,19 @@
+import config from "@/config/config.json";
 import { getListPage } from "@/lib/contentParser";
 import PageHeader from "@/partials/PageHeader";
 import SeoMeta from "@/partials/SeoMeta";
 import { RegularPage } from "@/types";
 import Share from "@/components/Share";
-import nodemailer from "nodemailer";
-
-require("dotenv").config();
 
 const Contact = async () => {
   const data: RegularPage = getListPage("contact/_index.md");
   const { frontmatter } = data;
   const { title, description, meta_title, image } = frontmatter;
-  
-  const handleFormSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-
-    const formData = new FormData (e.currentTarget);
-    const name= formData.get("name");
-    const email= formData.get("email");
-    const message= formData.get("message");
-
-    //trasporter
-    const transporter = nodemailer.createTransport({
-      service: "gmail",
-      secure: true,
-      auth: {
-        user: process.env.GMAIL_USER,
-        pass: process.env.GMAIL_PASS,
-      },
-    });
-
-    try {
-      // Envía el correo electrónico
-      await transporter.sendMail({
-        from: "tuCorreoDestino@gmail.com",
-        to: process.env.GMAIL_USER,
-        subject: "Nuevo mensaje del formulario de contacto",
-        text: `Nombre: ${name}\nCorreo electrónico: ${email}\nMensaje: ${message}`,
-      });
-
-      console.log("Correo electrónico enviado con éxito");
-    } catch (error) {
-      console.error("Error al enviar el correo electrónico:", error);
-    }
-  };
-
+  const { contact_form_action } = config.params;
 
   return (
     <>
-      <SeoMeta    
+      <SeoMeta
         title={title}
         meta_title={meta_title}
         description={description}
@@ -56,15 +21,13 @@ const Contact = async () => {
       />
       <PageHeader title={title} />
       <div className="flex items-center justify-center mt-10">
-                  <Share
-          className="social-icons"
-                   />
-                </div>
+        <Share className="social-icons" />
+      </div>
       <section className="section-sm">
         <div className="container">
           <div className="row">
             <div className="mx-auto md:col-10 lg:col-6 text-dark">
-              <form onSubmit={handleFormSubmit} >
+              <form action="https://formspree.io/f/xrgnqrkv" method="POST">
                 <div className="mb-6">
                   <label htmlFor="name" className="form-label">
                     Nombre completo <span className="text-red-500">*</span>
@@ -114,4 +77,3 @@ const Contact = async () => {
 };
 
 export default Contact;
-
